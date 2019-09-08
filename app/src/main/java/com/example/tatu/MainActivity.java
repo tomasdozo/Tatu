23a387b3a7b0bc14f6c53669285c6f50223c91cb
tomas <tomasdozo@hotmail.com>
2019-09-08 15:36:35 -0300

    08 sept

--- app/src/main/java/com/example/tatu/MainActivity.java
+++ app/src/main/java/com/example/tatu/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.tatu;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -18,24 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnJugar = findViewById(R.id.btnJugar);
         Button btnInstrucciones = findViewById(R.id.btnInstrucciones);
-        final EditText etNivel=findViewById(R.id.etNivel);
+
 
         btnJugar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!etNivel.getText().toString().isEmpty()) {
-                    int nivel = Integer.parseInt(etNivel.getText().toString());
-                    if (nivel > 0 && nivel < 5) {
-                        Intent intent = new Intent(MainActivity.this, com.example.tatu.Juego.class);
-                        intent.putExtra("nivel", nivel);
+                        Intent intent = new Intent(MainActivity.this, com.example.tatu.Niveles.class);
                         startActivity(intent);
-                    } else {
-                        Toast.makeText(MainActivity.this, "Nivel Inexistente", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "Ingrese un nivel entre 1 y 4", Toast.LENGTH_SHORT).show();
-                }
+
             }
         });
 
--- app/src/main/java/com/example/tatu/Niveles.java
+++ app/src/main/java/com/example/tatu/Niveles.java
@@ -1,14 +1,34 @@
 package com.example.tatu;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class Niveles extends AppCompatActivity {
+public class Niveles extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_niveles);
+        TableLayout tabla= findViewById(R.id.tabla);
+        for(int i=0; i<tabla.getChildCount();i++){
+            TableRow fila=(TableRow) tabla.getChildAt(i);
+            for (int j=0; j<fila.getChildCount();j++){
+                fila.getChildAt(j).setOnClickListener(this);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        String nroNivel=((TextView) view).getText().toString();
+        Intent intent = new Intent(Niveles.this, com.example.tatu.Juego.class);
+        intent.putExtra("nivel", nroNivel);
+        startActivity(intent);
     }
 }
