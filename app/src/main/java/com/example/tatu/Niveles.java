cabe07ea4782cb01567e420a4c545d1ab966aa3a
tomas <tomasdozo@hotmail.com>
2019-09-08 15:05:00 -0300

    08 sept

---
+++ app/src/main/java/com/example/tatu/Niveles.java
@@ -0,0 +1,14 @@
+package com.example.tatu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Niveles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_niveles);
+    }
+}
